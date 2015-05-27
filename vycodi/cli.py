dcec6a94cd88e8948f83e08e840b2fb2c1e83f1e
Paul Skopnik <paul@skopnik.me>
2015-05-27 23:04:22 +0200

    CLI: Added worker commands

--- vycodi/cli.py
+++ vycodi/cli.py
@@ -24,8 +24,31 @@ def statusHost(configFile):
 	else:
 		print("host daemon not running")
 
+
+@argh.named("start")
+def startWorker(configFile, foreground=False):
+	config = loadJSONConfig(configFile)
+	workerDaemon = WorkerDaemon.fromConfig(config, daemonize=not foreground)
+	workerDaemon.start()
+
+@argh.named("stop")
+def stopWorker(configFile):
+	config = loadJSONConfig(configFile)
+	workerDaemon = WorkerDaemon.fromConfig(config)
+	workerDaemon.stop()
+
+@argh.named("status")
+def statusWorker(configFile):
+	config = loadJSONConfig(configFile)
+	workerDaemon = WorkerDaemon.fromConfig(config)
+	if workerDaemon.status():
+		print("Worker daemon running")
+	else:
+		print("Worker daemon not running")
+
 parser = argh.ArghParser()
 parser.add_commands((startHost, stopHost, statusHost), namespace="host")
+parser.add_commands((startWorker, stopWorker, statusWorker), namespace="worker")
 
 def main():
 	parser.dispatch()
